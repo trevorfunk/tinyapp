c29c4cabd4074e075820c05aff0319e6589ea4c8
trevorfunk <trevorjonfunk@gmail.com>
2022-06-27 10:25:18 -0700

    add delete button

--- express_server.js
+++ express_server.js
@@ -37,6 +37,11 @@ app.post("/urls", (req, res) => {
  console.log(urlDatabase);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+ delete urlDatabase[req.params.shortURL];
+    res.redirect('/urls');
+})
+
 app.listen(PORT, () => {
  console.log(`Example app listening on port ${PORT}`);
 });
